86376db9a3cb9feab6042599883dfafb1832887b
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-11-08 16:58:14 +0400

    bug fix

--- z-route-controller-initializer.js
+++ z-route-controller-initializer.js
@@ -3,7 +3,7 @@
 module.exports = {
     run: function (app, next) {
         app.routeController = {};
-        app.utils.buildModulesInFolder(app, app.rc, app.folderPath.app.root + app.config.zRouteController.rootDir + '/');
+        app.utils.buildModulesInFolder(app, app.routeController, app.folderPath.app.root + app.config.zRouteController.rootDir + '/');
         next();
     }
 };
