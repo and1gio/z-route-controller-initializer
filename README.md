b7faa55b802ec5239b050a59a77f5f9d696da5b8
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-11-08 16:45:32 +0400

    rc to routeController

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # z-route-controller-initializer
+
+attaches *routeController* to *app*
+
+* app
+  * routeController
--- z-route-controller-initializer.js
+++ z-route-controller-initializer.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     run: function (app, next) {
-        app.rc = {};
+        app.routeController = {};
         app.utils.buildModulesInFolder(app, app.rc, app.folderPath.app.root + app.config.zRouteController.rootDir + '/');
         next();
     }
